d02a0af4793b01868080998b850b469157f94625
Romain <phenixstrato@gmail.com>
2019-12-09 17:09:13 +0100

    ajout de fonction asidepannel

--- src/modele/Livre.java
+++ src/modele/Livre.java
@@ -1,6 +1,5 @@
 package modele;
 
 public class Livre {
-
 	
 }
--- src/view/MainApp.java
+++ src/view/MainApp.java
@@ -2,21 +2,16 @@ package view;
 
 import java.awt.EventQueue;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JTabbedPane;
-
 import modele.ConnexionBDD;
-
 import javax.swing.JPanel;
 import java.awt.GridLayout;
 import java.awt.GridBagLayout;
-import javax.swing.JButton;
-import java.awt.GridBagConstraints;
-import java.awt.Color;
 
 public class MainApp {
 
 	private JFrame frame;
+
 	/**
 	 * Launch the application.
 	 */
@@ -48,7 +43,7 @@ public class MainApp {
 		frame.setBounds(100, 0, 1000, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new GridLayout(1, 0, 0, 0));
-		
+
 		ConnexionBDD.startConnection();
 
 		// AJOUT DU JTabbedPane
@@ -56,7 +51,7 @@ public class MainApp {
 		frame.getContentPane().add(tabbedPane);
 
 		// ====== DEBUT LIVRE PANEL ====== //
-		
+
 		JPanel livres_panel = new JPanel();
 		tabbedPane.addTab("Livres", null, livres_panel, null);
 		GridBagLayout gbl_panel = new GridBagLayout();
@@ -67,12 +62,12 @@ public class MainApp {
 		// === DEBUT HEADER LIVRE === //
 		String tabHeader[] = { "Titre", "ISBN", "Annee de sortie" };
 		double elmsSize[] = { 1.0, 1.0, 1.0, 1.0 };
-		HeaderPanel livre_header = new HeaderPanel(livres_panel, "Cet onglet permet de renseigner des livres", tabHeader,
-				elmsSize);
-    
+		HeaderPanel livre_header = new HeaderPanel(livres_panel, "Cet onglet permet de renseigner des livres",
+				tabHeader, elmsSize);
+
 		// === DEBUT MAIN LIVRE === //
 		MainPanel livre_main = new MainPanel(livres_panel);
-		//Add element
+		// Add element
 		livre_main.addModule(new LinkModule("Personne"), 0, 0);
 		livre_main.addModule(new Module(), 1, 0);
 		livre_main.addModule(new ImageModule(), 2, 0);
@@ -81,23 +76,25 @@ public class MainApp {
 		livre_main.addModule(new Module(), 1, 1);
 		livre_main.addModule(new Module(), 2, 1);
 		// == FIN MAIN == //
-		
+
 		// === DEBUT FOOTER LIVRE === //
-		String textBouton[] = {"Creer", "Modifier", "Supprimer"};
-		double elmsSizeFooter[] = {1.0, 1.0, 1.0};
+		String textBouton[] = { "Creer", "Modifier", "Supprimer" };
+		double elmsSizeFooter[] = { 1.0, 1.0, 1.0 };
 		FooterPanel livre_footer_panel = new FooterPanel(livres_panel, textBouton, elmsSizeFooter);
 		// == FIN FOOTER == //
 
 		// === DEBUT ASIDE LIVRE === //
 		AsidePanel livres_aside_panel = new AsidePanel(livres_panel);
-		livres_aside_panel.setDonnees(new Object[][] { { "La boussole d'or", null, null, null }, });
-		livres_aside_panel.ajouterLigne(new Object[] { "toto", null, null, null });
+		livres_aside_panel.setEntetes(new String[] { "Titre", "ISBN", "Genre", "Etat", "Annee" });
+		livres_aside_panel.setDonnees(new Object[][] { { "La boussole d'or", "toto en vacance", "valide", "2010" }, });
+		livres_aside_panel.ajouterLigne(new Object[] { "toto", "tata et toto", null, null });
+
 		// == FIN ASIDE == //
-		
+
 		// ===== FIN LIVRE ===== //
 
 		// ====== DEBUT FILMS PANEL ====== //
-		
+
 		JPanel films_panel = new JPanel();
 		tabbedPane.addTab("Films", null, films_panel, null);
 		films_panel.setLayout(new GridLayout(1, 0, 0, 0));
@@ -107,11 +104,11 @@ public class MainApp {
 		film_aside_panel.setDonnees(new Object[][] { { "seigneur des anneaux", null, null, null }, });
 		film_aside_panel.ajouterLigne(new Object[] { "toto", null, null, null });
 		// == FIN ASIDE == //
-		
+
 		// ===== FIN FILMS ===== //
 
 		// ====== DEBUT MUSIQUES PANEL ====== //
-		
+
 		JPanel musiques_panel = new JPanel();
 		tabbedPane.addTab("Musiques", null, musiques_panel, null);
 		musiques_panel.setLayout(new GridLayout(1, 0, 0, 0));
@@ -119,7 +116,7 @@ public class MainApp {
 		// ===== FIN MUSIQUES ===== //
 
 		// ====== DEBUT ARTISTES PANEL ====== //
-		
+
 		JPanel artistes_panel = new JPanel();
 		tabbedPane.addTab("Artistes", null, artistes_panel, null);
 		artistes_panel.setLayout(new GridLayout(1, 0, 0, 0));
@@ -127,7 +124,7 @@ public class MainApp {
 		// ===== FIN ARTISTES ===== //
 
 		// ====== DEBUT A VALIDER PANEL ====== //
-		
+
 		JPanel a_valider_panel = new JPanel();
 		tabbedPane.addTab("A valider", null, a_valider_panel, null);
 		a_valider_panel.setLayout(new GridLayout(1, 0, 0, 0));
@@ -135,7 +132,7 @@ public class MainApp {
 		// ====== FIN A VALIDER ===== //
 
 		// ====== DEBUT NETTOYAGE BDD PANEL ====== //
-		
+
 		JPanel netoyagebdd_panel = new JPanel();
 		tabbedPane.addTab("Nettoyage BDD", null, netoyagebdd_panel, null);
 		netoyagebdd_panel.setLayout(new GridLayout(1, 0, 0, 0));
