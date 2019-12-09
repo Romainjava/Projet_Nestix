6cd2eaca8017723ad0fbb78709a0272a18013ac2
Thibault Cougrand <thibault.cougrand@gmail.com>
2019-12-09 12:49:57 +0100

    refacto commentaires

--- src/view/MainApp.java
+++ src/view/MainApp.java
@@ -46,12 +46,12 @@ public class MainApp {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new GridLayout(1, 0, 0, 0));
 
-		// === ICI AJOUT DU JTabbedPane === //
+		// AJOUT DU JTabbedPane
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		frame.getContentPane().add(tabbedPane);
-		// === FIN AJOUT DU JTabbedPane === //
 
-		// === ICI AJOUT LIVRE PANEL === //
+		// ====== DEBUT LIVRE PANEL ====== //
+		
 		JPanel livres_panel = new JPanel();
 		tabbedPane.addTab("Livres", null, livres_panel, null);
 		GridBagLayout gbl_panel = new GridBagLayout();
@@ -59,14 +59,14 @@ public class MainApp {
 		gbl_panel.rowWeights = new double[] { 1.0, 3.5, 0.5 };
 		livres_panel.setLayout(gbl_panel);
 
-		// === ICI HEADER PANEL LIVRE === //
+		// === DEBUT HEADER LIVRE === //
 		String tabHeader[] = { "Titre", "ISBN", "Annee de sortie" };
 		double elmsSize[] = { 1.0, 1.0, 1.0, 1.0 };
 		HeaderPanel livre_header = new HeaderPanel(livres_panel, "Cet onglet permet de renseigner des livres", tabHeader,
 				elmsSize);
-		// === FIN HEADER PANEL LIVRE === //
+		// == FIN HEADER == //
 		
-		// === ICI MAIN PANEL LIVRE === //
+		// === DEBUT MAIN LIVRE === //
 		MainPanel livre_main = new MainPanel(livres_panel);
 		//Add element
 		livre_main.addModule(new LinkModule("Personne"), 0, 0);
@@ -76,61 +76,67 @@ public class MainApp {
 		livre_main.addModule(new LinkModule("Genre"), 0, 1);
 		livre_main.addModule(new Module(), 1, 1);
 		livre_main.addModule(new Module(), 2, 1);
-		// === FIN MAIN PANEL LIVRE === //
+		// == FIN MAIN == //
 		
-		// === ICI FOOTER PANEL LIVRE === //
+		// === DEBUT FOOTER LIVRE === //
 		String textBouton[] = {"Cr�er", "Modifier", "Supprimer"};
 		double elmsSizeFooter[] = {1.0, 1.0, 1.0};
 		FooterPanel livre_footer_panel = new FooterPanel(livres_panel, textBouton, elmsSizeFooter);
-		// === FIN FOOTER PANEL LIVRE === //
+		// == FIN FOOTER == //
 
-		// === ICI ASIDE LIVRE PANEL === //
+		// === DEBUT ASIDE LIVRE === //
 		AsidePanel livres_aside_panel = new AsidePanel(livres_panel);
 		livres_aside_panel.setDonnees(new Object[][] { { "La boussole d'or", null, null, null }, });
 		livres_aside_panel.ajouterLigne(new Object[] { "toto", null, null, null });
-		// === FIN LIVRE ASIDE === //
-		// === FIN LIVRE PANEL === //
+		// == FIN ASIDE == //
+		
+		// ===== FIN LIVRE ===== //
 
-		// === ICI AJOUT FILMS PANEL === //
+		// ====== DEBUT FILMS PANEL ====== //
+		
 		JPanel films_panel = new JPanel();
 		tabbedPane.addTab("Films", null, films_panel, null);
 		films_panel.setLayout(new GridLayout(1, 0, 0, 0));
-
 
 		// === DEBUT ASIDE FILMS === //
 		AsidePanel film_aside_panel = new AsidePanel(films_panel);
 		film_aside_panel.setDonnees(new Object[][] { { "seigneur des anneaux", null, null, null }, });
 		film_aside_panel.ajouterLigne(new Object[] { "toto", null, null, null });
-		// === FIN ASIDE FILMS === //
-		// === ICI FIN FILMS PANEL === //
+		// == FIN ASIDE == //
+		
+		// ===== FIN FILMS ===== //
 
-		// === ICI MUSIQUES PANEL === //
+		// ====== DEBUT MUSIQUES PANEL ====== //
+		
 		JPanel musiques_panel = new JPanel();
 		tabbedPane.addTab("Musiques", null, musiques_panel, null);
 		musiques_panel.setLayout(new GridLayout(1, 0, 0, 0));
 
-		// === ICI FIN MUSIQUES PANEL === //
+		// ===== FIN MUSIQUES ===== //
 
-		// === ICI ARTISTES PANEL === //
+		// ====== DEBUT ARTISTES PANEL ====== //
+		
 		JPanel artistes_panel = new JPanel();
 		tabbedPane.addTab("Artistes", null, artistes_panel, null);
 		artistes_panel.setLayout(new GridLayout(1, 0, 0, 0));
 
-		// === ICI FIN ARTISTES PANEL === //
+		// ===== FIN ARTISTES ===== //
 
-		// === ICI A VALIDER PANEL === //
+		// ====== DEBUT A VALIDER PANEL ====== //
+		
 		JPanel a_valider_panel = new JPanel();
 		tabbedPane.addTab("A valider", null, a_valider_panel, null);
 		a_valider_panel.setLayout(new GridLayout(1, 0, 0, 0));
 
-		// === ICI FIN A VALIDER PANEL === //
+		// ====== FIN A VALIDER ===== //
 
-		// === ICI AJOUT NETTOYAGE BDD === //
+		// ====== DEBUT NETTOYAGE BDD PANEL ====== //
+		
 		JPanel netoyagebdd_panel = new JPanel();
 		tabbedPane.addTab("Nettoyage BDD", null, netoyagebdd_panel, null);
 		netoyagebdd_panel.setLayout(new GridLayout(1, 0, 0, 0));
 
-		// === ICI FIN AJOUT NETTOYAGE BDD === //
+		// ===== FIN NETTOYAGE BDD ===== //
 
 	}
 }
