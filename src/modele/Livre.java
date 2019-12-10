276b178269548b78494612d185f0e3af202e148f
Vincent Landrieux <vincent.landrieux@gmail.com>
2019-12-10 23:46:29 +0100

    Update Livre.java
    Add BDD pull data

--- src/modele/Livre.java
+++ src/modele/Livre.java
@@ -1,5 +1,9 @@
 package modele;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class Livre extends Media {
@@ -62,8 +66,47 @@ public class Livre extends Media {
 
 	@Override
 	public boolean lireUn(int id) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean success = true;
+		try {
+			String query = "SELECT * FROM nestix_media JOIN nestix_livre ON id_media = livre_id WHERE id_media = " + id;
+			Statement state = ConnexionBDD.startConnection().createStatement();
+			ResultSet result = state.executeQuery(query);
+			
+			ResultSetMetaData resultMeta = result.getMetaData();
+		    System.out.println("\n**********************************");
+		    for(int i = 1; i <= resultMeta.getColumnCount(); i++) {
+		      System.out.print(resultMeta.getColumnName(i).toUpperCase() + " *");
+		    }
+		    System.out.println("\n**********************************");
+		    while(result.next()){         
+		      for(int i = 1; i <= resultMeta.getColumnCount(); i++) {
+		    	  if(result.getObject(i) != null) {
+		    		  System.out.print(result.getObject(i).toString() + "\t |");
+		    	  }else {
+		    		  System.out.print("NULL" + "\t |");
+		    	  }
+		      }
+		      System.out.println("\n---------------------------------");
+		    }
+		    result.absolute(1);
+		    if(result.getObject(12) != null) {
+		    	System.out.println(result.getObject(12).toString());
+		    	setISBN(result.getInt(12));
+		    }else {
+		    	System.out.println("NULL");
+		    	setISBN(result.getInt(12));
+		    }
+		    
+		    System.out.println(getISBN());
+	
+		    result.close();
+		    state.close();
+			
+		} catch (Exception e) {
+			System.out.println("erreur");
+			success = false;
+		}
+		return success;
 	}
 
 	@Override
@@ -82,6 +125,27 @@ public class Livre extends Media {
 	public boolean recherchePar(int limit) {
 		// TODO Auto-generated method stub
 		return false;
+	}
+	
+	
+	public static void main(String[] args) {
+		Livre livre = new Livre();
+		
+		livre.lireUn(1);
+		
+//		ConnexionBDD.startConnection();
+//		musique.setTitre_media("test");
+//		Genre genre = new Genre();
+//		genre.setNom_genre("rock");
+//		Genre genre2 = new Genre();
+//		genre2.setNom_genre("pop");
+//		musique.addGenre(genre);
+//		musique.addGenre(genre2);
+//		ConnexionBDD.startConnection();
+//		musique.setAnnee_sortie_media("1060");
+//		for (String iterable_element : musique.toRowData()) {
+//			System.out.println(iterable_element);
+//		}
 	}
 
 }
