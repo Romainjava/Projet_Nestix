0180b33918842752474c7d1deedf73b72b5801a7
kevinSanMartino <kevinsanmartino@gmail.com>
2019-12-12 11:56:05 +0100

    ajout image class

---
+++ src/modele/Image.java
@@ -0,0 +1,60 @@
+package modele;
+
+import java.util.ArrayList;
+
+public class Image extends Info {
+
+	private String path_image;
+	private String alt_image;
+	@Override
+	public boolean suppression(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ArrayList<I_recherche> lectureTout(int limit) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean recherchePar(int limit) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object[] toRowData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] toHeaderData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String getTableName() {
+		return "image";
+	}
+
+	public String getPath_image() {
+		return path_image;
+	}
+
+	public void setPath_image(String path_image) {
+		this.path_image = path_image;
+	}
+
+	public String getAlt_image() {
+		return alt_image;
+	}
+
+	public void setAlt_image(String alt_image) {
+		this.alt_image = alt_image;
+	}
+
+}
