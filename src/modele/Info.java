1ff8fed060cf208ef2eec812ae0811784d34add5
Romain <phenixstrato@gmail.com>
2019-12-13 10:54:35 +0100

    Update Info.java

--- src/modele/Info.java
+++ src/modele/Info.java
@@ -6,76 +6,79 @@ import java.sql.SQLException;
 
 public abstract class Info implements I_requeteSQL, I_recherche {
 
-
 	protected int id;
 	protected String nom;
+
 	abstract protected String getTableName();
 
-
-
 	public Info() {
-		
+
 	}
+
 	public Info(int pId, String pNom) {
 		this.id = pId;
 		this.nom = pNom;
 
 	}
+
 	protected String getColumnName() {
 		return "nom_" + this.getTableName();
 	}
+
 	protected String getIdName() {
 		return "id_" + this.getTableName();
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public String getNom() {
 		return nom;
 	}
-	
+
 	public String[] toRowData() {
 		String[] data = { this.nom };
 		return data;
 	}
 
-	
 	public String[] toHeaderData() {
 		String[] data = { this.getTableName() };
 		return data;
 	}
+
 	public void fetchId() {
-			PreparedStatement statement;
-			ResultSet result;
-			String query;
-			try {
-				if(this.nom==null) {
-					throw new Exception("nom obligatoire");
-				}
-				query = "SELECT " + this.getIdName() + " FROM nestix_" + this.getTableName() + " WHERE "
-						+ this.getColumnName() + "=?";
-				statement = (PreparedStatement) ConnexionBDD.getConnexion().prepareStatement(query);
-				statement.setString(1, this.nom);
-				result = statement.executeQuery();				
-				if (result.next()) {
-					this.id = result.getInt(1);
-				}
-				if(result.getRow() == 0) {
-					System.out.println("id non trouvé class info ligne 62");
-				}
-				statement.close();
-			} catch (Exception e) {
-				e.printStackTrace();
+		PreparedStatement statement;
+		ResultSet result;
+		String query;
+		try {
+			if (this.nom == null) {
+				throw new Exception("nom obligatoire");
 			}
+			query = "SELECT " + this.getIdName() + " FROM nestix_" + this.getTableName() + " WHERE "
+					+ this.getColumnName() + "=?";
+			statement = (PreparedStatement) ConnexionBDD.getConnexion().prepareStatement(query);
+			statement.setString(1, this.nom);
+			result = statement.executeQuery();
+			if (result.next()) {
+				this.id = result.getInt(1);
+			}
+			if (result.getRow() == 0) {
+				System.out.println("id non trouvé class info ligne 62");
+			}
+			statement.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void setId(int id) {
 		this.id = id;
 	}
-	
+
 	public void setInfo(ResultSet result) {
 		try {
-			this.setId(result.getInt(this.getTableName()+"_id"));
+			this.setId(result.getInt(this.getTableName() + "_id"));
 			this.setNom(result.getString("nom_" + this.getTableName()));
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -83,38 +86,38 @@ public abstract class Info implements I_requeteSQL, I_recherche {
 	}
 
 	public void fetchNom() {
-			PreparedStatement statement;
-			ResultSet result;
-			String query;
-			try {
-				if(this.id==0) {
-					throw new Exception("id obligatoire");
-				}
-				query = "SELECT " + this.getColumnName() + " FROM nestix_" + this.getTableName() + " WHERE "
-						+ this.getIdName() + "=?";
-				statement = (PreparedStatement) ConnexionBDD.getConnexion().prepareStatement(query);
-				statement.setInt(1, this.id);
-				result = statement.executeQuery();
-				if (result.next()) {
-					this.nom = result.getString(1);
-				}
-				statement.close();
-			} catch (Exception e) {
-				e.printStackTrace();
+		PreparedStatement statement;
+		ResultSet result;
+		String query;
+		try {
+			if (this.id == 0) {
+				throw new Exception("id obligatoire");
 			}
+			query = "SELECT " + this.getColumnName() + " FROM nestix_" + this.getTableName() + " WHERE "
+					+ this.getIdName() + "=?";
+			statement = (PreparedStatement) ConnexionBDD.getConnexion().prepareStatement(query);
+			statement.setInt(1, this.id);
+			result = statement.executeQuery();
+			if (result.next()) {
+				this.nom = result.getString(1);
+			}
+			statement.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
-	
-	
+
 	@Override
 	public String toString() {
 		return this.getTableName() + " [id=" + id + ", nom=" + nom + "]";
 	}
+
 	public String toStringCustom() {
-		return nom ;
+		return nom;
 	}
 
 	public boolean creation() {
@@ -131,8 +134,9 @@ public abstract class Info implements I_requeteSQL, I_recherche {
 			if (result.next()) {
 				id = result.getInt(1);
 				this.id = id;
-			//	System.out.println("création de "+this.nom+"dans la table "+this.getTableName());
-			}else {
+				// System.out.println("création de "+this.nom+"dans la table
+				// "+this.getTableName());
+			} else {
 				this.fetchId();
 			}
 			statement.close();
@@ -144,13 +148,13 @@ public abstract class Info implements I_requeteSQL, I_recherche {
 	}
 
 	public void setInfo(String nom) {
-		this.nom=nom;
+		this.nom = nom;
 		this.fetchId();
-		if(this.getId()==0 && !this.getNom().equals("")) {
+		if (this.getId() == 0 && !this.getNom().equals("")) {
 			this.creation();
 		}
 	}
-	
+
 	public boolean lireUn(int id) {
 		PreparedStatement statement;
 		ResultSet result;
@@ -193,8 +197,9 @@ public abstract class Info implements I_requeteSQL, I_recherche {
 		return (result > 0);
 
 	}
+
 	public boolean supression() {
-		//TODO
+		// TODO
 		return false;
 	}
 
